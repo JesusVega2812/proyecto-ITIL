3bbf79aa1c7efb4f881f4928c1c2d27260d7106f
Jose <jose_alberto2233@hotmail.com>
2024-09-26 21:52:03 -0700

    Componente edificios actualizado

---
+++ src/Componets/Edificios/Edificios Query.sql
@@ -0,0 +1,14 @@
+﻿use proyectoITIL
+
+create PROCEDURE ActualizarEdificio
+    @id_edificio INT,
+    @nombre NVARCHAR(100),
+    @ubicacion_edificio NVARCHAR(100)
+AS
+BEGIN
+    -- Actualizar el edificio con los nuevos valores
+    UPDATE EDIFICIO
+    SET nombre = @nombre,
+        ubicacion_edificio = @ubicacion_edificio
+	WHERE id_edificio = @id_edificio;
+END;
