9f48b117dd6401c80e317a0b9300fa06c55d0df1
JesusVega2812 <l201708582@culiacan.tecnm.mx>
2024-09-26 22:34:22 -0700

    JAJAJAJAJAJA

--- src/Componets/Equipos/Equipo_bodega.jsx
+++ src/Componets/Equipos/Equipo_bodega.jsx
@@ -226,6 +226,18 @@ export const EquipoBodega = () => {
                     </div>
                 )}
 
+                {(radioCheckEquipo === 'Computadora') && (
+                    <div className='EquipoBodega-form-columns'>
+                        <label className='form-label'>Softwares</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Default checkbox
+                            </label>
+                        </div>
+                    </div>
+                )}
+
                 {(radioCheckEquipo === 'Impresora') && (
                     <div className="EquipoBodega-form-columns">
                         <div className="EquipoBodega-column">
